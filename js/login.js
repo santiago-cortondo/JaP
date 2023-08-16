e1188616318edc8f2552475de8ab99bf8a44878e
German Maximo Montero Sande <german.montero@fing.edu.uy>
2023-08-16 14:27:52 -0300

    cambios y arreglos

--- js/login.js
+++ js/login.js
@@ -1,14 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("login").addEventListener("click", () => {
+    document.getElementById("login").addEventListener("click", e => {
+        //e.preventDefault();
+
+        const rember = document.getElementById("inputCheck").checked;
+
         const mail = document.getElementById("inputEmail").value;
         const pass = document.getElementById("inputPassword").value;
 
         if (mail.length > 0 && pass.length > 0) {
             try {
-                localStorage.setItem("user", JSON.stringify({ 'mail': mail, 'pass': pass }));
+                if(rember)
+                    localStorage.setItem("user", JSON.stringify({ 'mail': mail, 'pass': pass }));
+                else
+                    sessionStorage.setItem("user", JSON.stringify({ 'mail': mail, 'pass': pass }));
             } catch (e) {
             }
             location.href = "./index.html";
+        } else {//inalcanzable, el atributo 'requiered' es verificacion suficiente
+            Swal.fire("Error!", "Verifique email y contraseña", "error");
+            //alert("Verifique email y contraseña")
         }
     });
 });
