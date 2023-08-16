0f253e8014bbf3c7c10a4db9c44f8103baefe78b
German Maximo Montero Sande <german.montero@fing.edu.uy>
2023-08-16 15:26:29 -0300

    ultimos arreglos

--- js/login.js
+++ js/login.js
@@ -7,17 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
         const mail = document.getElementById("inputEmail").value;
         const pass = document.getElementById("inputPassword").value;
 
+        const session = {
+            'mail': mail,
+            'pass': pass
+        };
+
+        //broken, https://stackoverflow.com/questions/36659605/sessionstorage-cleaned-up-unexpectedly-on-android
+        //const stor = rember ? localStorage : sessionStorage;
+        const stor = localStorage;
+
         if (mail.length > 0 && pass.length > 0) {
             try {
-                if(rember)
-                    localStorage.setItem("user", JSON.stringify({ 'mail': mail, 'pass': pass }));
-                else
-                    sessionStorage.setItem("user", JSON.stringify({ 'mail': mail, 'pass': pass }));
+                stor.setItem("user", JSON.stringify(session));
             } catch (e) {
             }
             location.href = "./index.html";
-        } else {//inalcanzable, el atributo 'requiered' es verificacion suficiente
-            Swal.fire("Error!", "Verifique email y contraseña", "error");
+        } else {//Inalcanzable?, el atributo 'requiered' debería ser verificación suficiente
+            Swal.fire("Error!", "Verifique email y contraseña\n(no pueden ser vacios)", "error");
             //alert("Verifique email y contraseña")
         }
     });
