ec2037de65fc180cd268e91492fb16f4380ac72e
German Maximo Montero Sande <german.montero@fing.edu.uy>
2023-08-19 18:38:25 -0300

    aa

--- js/login.js
+++ js/login.js
@@ -10,18 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
             'mail': mail,
             'pass': pass
         };
-
-        //broken, https://stackoverflow.com/questions/36659605/sessionstorage-cleaned-up-unexpectedly-on-android
-        //const stor = rember ? localStorage : sessionStorage;
-        const stor = localStorage;
-
+        
         if (mail.length > 0 && pass.length > 0) {
             try {
-                stor.setItem("user", JSON.stringify(session));
+                localStorage.setItem("user", JSON.stringify(session));
             } catch (e) {
             }
             location.href = "./index.html";
-        } else {//Inalcanzable?, el atributo 'requiered' debería ser verificación suficiente
+        } else {
             Swal.fire("Error!", "Verifique email y contraseña\n(no pueden ser vacios)", "error");
             //alert("Verifique email y contraseña")
         }
