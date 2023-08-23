77c31a17c2f127cda705d27ad326b9a8adbcb482
German Maximo Montero Sande <german.montero@fing.edu.uy>
2023-08-23 13:49:05 -0300

    barar de busqueda, no funca?

--- js/products.js
+++ js/products.js
@@ -1,22 +1,44 @@
 let URL_InfoAutos = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
-  
+let array = [];
+
 fetch(URL_InfoAutos)
-.then(res => res.json())
-.then(res => {
+    .then(res => res.json())
+    .then(res => {
+        array = res.products;
+        MostrarData(res.products)
+    })
 
-MostrarData(res.products)
-})
-
-let productos=document.getElementById("productos");
+let productos = document.getElementById("productos");
 
 function MostrarData(dataArray) {
+    productos.innerHTML += "";
+    for (const item of dataArray) {
+        productos.innerHTML += `
+        <div class="cuadrante">
+            <img src="${item.image}">
+            <div>
+                <h2>${item.name} ${item.currency} ${item.cost}</h2>
+                <p class ="descripcion">${item.description}</p>
+            </div>
+            <div class ="vendidos">
+                <span>${item.soldCount} vendidos</span>
+            </div>
+        </div>`;
+    }
+}
 
-   for (const item of dataArray) {
-     
-     productos.innerHTML += `<div class="cuadrante"> <img src="${item.image}"> <div><h2>${item.name} 
-     ${item.currency} ${item.cost}</h2> <p class ="descripcion"> ${item.description} </p></div> <div class ="vendidos"><span>${item.soldCount} vendidos</span></div>
-     
-     </div>`; 
-   }
- }
+document.addEventListener("DOMContentLoaded", () => {
+    const s = document.getElementById("search");
+    s.addEventListener("input", () => {
+        const val = s.value;
+        let fil = array.filter(e => e.name.includes(val) || e.description.includes(val))
+        MostrarData(fil);
+    });
+});
+
+/*
+
+que filtre en tiempo real (el evento input te será de ayuda) según el texto que se ingresa en dicho campo.
+Incluye en la búsqueda el texto en el título y en la descripción de los artículos.
+*/
